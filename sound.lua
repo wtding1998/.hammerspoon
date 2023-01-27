e563fcb8e2507cb3e35c62f69b983c93584a7692
Wentao <wtding1998@gmail.com>
2023-01-27 21:03:19 +0800

    add toggle device key

--- sound.lua
+++ sound.lua
@@ -11,7 +11,7 @@ function changeVolume(diff)
   end
 end
 
-local soundHyper = {'option', 'ctrl'}
+local soundHyper = {'option', 'shift'}
 hs.hotkey.bind(soundHyper, 'j', changeVolume(-5))
 hs.hotkey.bind(soundHyper, 'k', changeVolume(5))
 
@@ -25,26 +25,62 @@ function toggleVolume()
   end
 end
 
+local function nextOutput()
+    local currDevice = hs.audiodevice.defaultOutputDevice()
+    local outputDevices = hs.audiodevice.allOutputDevices()
+    local index = 0
+    for i, device in pairs(outputDevices) do
+        if device == currDevice then
+            index = i
+            break
+        end
+    end
+    if index >= #outputDevices then
+        index = 0
+    end
+    outputDevices[index + 1]:setDefaultOutputDevice()
+    hs.alert.show("Output: " .. outputDevices[index + 1]:name(), {}, 0.5)
+end
+local function nextInput()
+    local currDevice = hs.audiodevice.defaultInputDevice()
+    local inputDevices = hs.audiodevice.allInputDevices()
+    local index = 0
+    for i, device in pairs(inputDevices) do
+        if device == currDevice then
+            index = i
+            break
+        end
+    end
+    if index >= #inputDevices then
+        index = 0
+    end
+    inputDevices[index + 1]:setDefaultInputDevice()
+    hs.alert.show("Input: " .. inputDevices[index + 1]:name(), {}, 0.5)
+end
+
 hs.hotkey.bind(soundHyper, 'm', function() toggleVolume() end)
+hs.hotkey.bind(soundHyper, "space", nextOutput)
+hs.hotkey.bind(soundHyper, "/", nextInput)
 
 -- copied from https://gist.github.com/huytd/20f98ecb2bf1d76b4de9f6c21f5fb37c
 -- hyper + l = Next song
-hs.hotkey.bind(soundHyper, "l", function()
+hs.hotkey.bind(soundHyper, "[", function()
   hs.eventtap.event.newSystemKeyEvent('NEXT', true):post()
   hs.eventtap.event.newSystemKeyEvent('NEXT', false):post()
 end)
 
 -- hyper + h = Prev song
-hs.hotkey.bind(soundHyper, "h", function()
+hs.hotkey.bind(soundHyper, "]", function()
   hs.eventtap.event.newSystemKeyEvent('PREVIOUS', true):post()
   hs.eventtap.event.newSystemKeyEvent('PREVIOUS', false):post()
 end)
 
 -- hyper + space = Play / Pause
-hs.hotkey.bind(soundHyper, "space", function()
+hs.hotkey.bind(soundHyper, "p", function()
   hs.eventtap.event.newSystemKeyEvent('PLAY', true):post()
   hs.eventtap.event.newSystemKeyEvent('PLAY', false):post()
 end)
+
 
 -- local hyper     = {"ctrl", "alt", "cmd"}
 -- local lesshyper = {"ctrl", "alt"}
